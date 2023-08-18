55c3cc33f67981f8958a73b326daf20618104e89
PedroMorais <95058545+PedroMorais2703@users.noreply.github.com>
2023-08-17 21:29:42 -0300

    Create script-intro-função.js

---
+++ seção04/ex02/script-intro-função.js
@@ -0,0 +1,23 @@
+function somar(num1, num2){
+    var res = num1 + num2;
+
+    return res;
+}
+ var resultado = somar(6, 10);
+ console.log(resultado);
+
+
+function mensagem(){
+    console.log('Está é minha mensagem');
+
+}
+
+mensagem()
+
+function multiplicar(n1, n2){
+    
+    
+    return n1 * n2;
+}
+var r = multiplicar(1, 2);
+console.log(r);
