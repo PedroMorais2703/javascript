b2782ca394e718f2d2fa95428bae04e032d3aa59
PedroMorais <95058545+PedroMorais2703@users.noreply.github.com>
2023-10-25 22:04:19 -0300

    array

---
+++ seção04/ex02/script-escopo-veriavel.js
@@ -0,0 +1,10 @@
+function somar(num1, num2) {
+    var valor = 5; //variavel local
+    return valor + num1 + num2;
+}
+
+console.log(somar(1, 2));
+
+var outra = 5 // variavel global
+
+console.log(outra);
---
+++ seção05/aula02array.js
@@ -0,0 +1,16 @@
+// declarando array
+
+//forma1
+var alunos = new Array('a', 'b', 'c', 'd');
+console.log(alunos);
+
+//forma2 recomendada
+var notas = [1, 2, 3, 4];
+console.log(notas);
+
+//fazendo acesso ao valor de um indice
+console.log(notas[2])
+
+//alterando o valor a partir do indice
+notas[2] = 12;
+console.log(notas);
