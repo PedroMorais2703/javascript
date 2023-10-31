9f041d3ba796161b9c1f5294bd9945f0a449407d
PedroMorais <95058545+PedroMorais2703@users.noreply.github.com>
2023-10-30 22:47:45 -0300

    Update aula03foreach.js

--- seção05/aula03foreach.js
+++ seção05/aula03foreach.js
@@ -1,9 +1,14 @@
 var nomes = ['paula', 'maria', 'pedro', 'beatriz'];
  
 // definir uma função
-function imprimir(nomes, indice) {
+function imprimir(nomes, indice, array) {
     console.log(indice + '-' + nomes);
 }
 
 //para cada (traducao)
 nomes.forEach(imprimir);
+
+// ultilização de uma função anonima (lambida/callback) e tamplete string
+nomes.forEach(function(nomes, indice){
+    console.log(`${indice} - ${nomes}`)
+});
