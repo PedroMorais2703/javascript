77d106aca7b53be5516b09bf972d530c5969c412
PedroMorais <95058545+PedroMorais2703@users.noreply.github.com>
2023-12-27 21:20:54 -0300

    Create arrow-functions.js

---
+++ seção06/arrow-functions.js
@@ -0,0 +1,13 @@
+ let somar4 = (num1, num2) => {
+    return num1 + num2;
+ }
+
+ console.log(somar4(4,6));
+
+ /* Caso a sua função tenha apenas 1 parametro de entreda e executar apenas uma linha,
+ você pode simplificar ainda mais usando Arroe Function
+ */
+
+let dobrar = valor => valor * 2
+
+console.log(dobrar(5));
