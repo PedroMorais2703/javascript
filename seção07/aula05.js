f250ba74021e468765debe4f6506fd4e1ed3632f
PedroMorais <95058545+PedroMorais2703@users.noreply.github.com>
2024-02-02 21:00:19 -0300

    Update aula05.js

--- seção07/aula05.js
+++ seção07/aula05.js
@@ -1,7 +1,35 @@
 class Pessoa{
     constructor(nome, sobrenome){
-        this.nome = nome;
-        this.sobrenome = sobrenome;
+        this._nome = nome;
+        this._sobrenome = sobrenome;
+    }
+
+    get nome(){
+        return this._nome;
+    }
+
+    set nome(nome){
+        return this._nome = nome;
+    }
+
+    get sobrenome(){
+        return this._sobrenome;
+    }
+
+    set sobrenome(sobrenome){
+        return this._sobrenome = sobrenome;
+    }
+
+    falar(msg){
+        console.log(`${this.nome} está falnado ${msg}`);
+    }
+
+    comer(){
+        console.log(`${this.nome} está comendo`);
+    }
+
+    beber(msg){
+        console.log(`${this.nome} ${this.sobrenome} está bebendo`);
     }
 }
 
@@ -9,6 +37,22 @@ class Carro {
     constructor(modelo){
         this.marca = 'Honda';
         this.modelo = modelo;
+    }
+
+    imprimir(){
+        console.log(`${this.marca} ${this.modelo}`);
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    get modelo(){
+        return this._modelo;
+    }
+
+    set modelo(modelo){
+        return this._modelo = modelo;
     }
 
 }
