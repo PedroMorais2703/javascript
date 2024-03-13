ae41b6e43bbfa4e5755a297fd485df574ba54e05
PedroMorais <95058545+PedroMorais2703@users.noreply.github.com>
2024-03-13 20:58:57 -0300

    Create aula03.js

---
+++ seção10/aula03.js
@@ -0,0 +1,12 @@
+// conjuntos não aceitam repetição de valores 
+// conjuntos não são indexados;
+
+//declarando um conjunto
+let cursos = new Set();
+
+//adicionando valores
+
+cursos.add('Programação')
+
+//adiconando valores concatenados
+cursos.add('programação em c').add('logica de programação').add('programação em python')
